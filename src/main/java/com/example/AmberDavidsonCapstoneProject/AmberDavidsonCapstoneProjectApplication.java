bce4c940da217e70070c7bddf5949c10be0608f3
amberdavidson <david575@umn.edu>
2022-03-17 20:50:53 -0500

    add files from desktop

---
+++ src/main/java/com/example/AmberDavidsonCapstoneProject/AmberDavidsonCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmberDavidsonCapstoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmberDavidsonCapstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmberDavidsonCapstoneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AmberDavidsonCapstoneProject/AmberDavidsonCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmberDavidsonCapstoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmberDavidsonCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
